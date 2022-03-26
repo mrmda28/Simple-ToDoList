e96eb639beb4084811d2241bf851fbcaff1bc952
mrmda28 <mrmda28@gmail.com>
2022-03-26 15:07:36 +0300

    Initial Commit

---
+++ SimpleToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleToDoList
+//
+//  Created by Dmitriy Maslennikov on 26/03/2022.
+//  Copyright © 2022 Dmitry Maslennikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
